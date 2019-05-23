181a389c2193f9326c22ee4c8729a90dc6b87bc2
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-05-23 07:32:06 +0000

    Added logout

--- android/app/src/main/java/io/sotads/core/firebase/ADSMessagingService.kt
+++ android/app/src/main/java/io/sotads/core/firebase/ADSMessagingService.kt
@@ -1,10 +1,13 @@
 package io.sotads.core.firebase
 
+import android.content.Context
 import android.content.Intent
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import io.codelabs.sdk.util.debugLog
 import io.sotads.R
+import io.sotads.core.util.PREFS_NAME
+import io.sotads.core.util.USER_KEY
 import io.sotads.core.util.createNotificationChannel
 import io.sotads.core.util.pushNotification
 import io.sotads.view.AccidentActivity
@@ -17,13 +20,19 @@ class ADSMessagingService : FirebaseMessagingService() {
 
             createNotificationChannel(data["title"] ?: getString(R.string.notification_channel_name))
 
-            pushNotification(
-                data["title"],
-                data["Tap here for more details"],
-                Intent(applicationContext, AccidentActivity::class.java).apply {
-                    putExtra(AccidentActivity.ACCIDENT_KEY, data["key"])
-                }
-            )
+            if (!applicationContext.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).getString(
+                    USER_KEY,
+                    null
+                ).isNullOrEmpty()
+            ) {
+                pushNotification(
+                    data["title"],
+                    data["Tap here for more details"],
+                    Intent(applicationContext, AccidentActivity::class.java).apply {
+                        putExtra(AccidentActivity.ACCIDENT_KEY, data["key"])
+                    }
+                )
+            }
         }
     }
 
